5a2836102f01995ee1fe563c1c0b9163607e1f62
Zefirchiky <mudruk.arte@gmail.com>
2024-10-02 02:28:59 +0200

    Init

---
+++ mangareader/config/__init__.py
@@ -0,0 +1,6 @@
+'''
+Project settings:
+- settings.py parsing other config files
+'''
+
+from settings import *
---
+++ mangareader/scraper/scraper.py
@@ -0,0 +1,12 @@
+from mangareader.config.settings import *
+
+
+class Scraper:
+    def __init__(self):
+        pass
+
+    def get_chapters(self):
+        pass
+
+    def get_pages(self):
+        pass
